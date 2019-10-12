a4d1c1f2a800948cf0fc783f2ccc1792c5931260
chengjun <wangchj04@gmail.com>
2019-10-12 14:04:53 +0800

    master 3

--- _site/serve.sh
+++ _site/serve.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# open github master branch
+# open atom master branch
 cd ..
 jupyter-book build ccbook/
 cd ccbook
 make serve
 
-# Publish your book's HTML manually to GitHub-pages 
+# Publish your book's HTML manually to GitHub-pages
+# publish the _site folder of the master branch to the gh-pages branch
 # ghp-import -n -p -f _site
