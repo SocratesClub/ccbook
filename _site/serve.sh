61fda3d461c74dde8498a3fab1fba142b4c3b30e
chengjun <wangchj04@gmail.com>
2019-10-12 11:54:02 +0800

    master2

--- _site/serve.sh
+++ _site/serve.sh
@@ -3,3 +3,6 @@ cd ..
 jupyter-book build ccbook/
 cd ccbook
 make serve
+
+# Publish your book's HTML manually to GitHub-pages 
+# ghp-import -n -p -f _site
--- serve.sh
+++ serve.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ..
-jupyter-book build ccbook-source/
-cd ccbook-source
+jupyter-book build ccbook/
+cd ccbook
 make serve
+
+# Publish your book's HTML manually to GitHub-pages 
+# ghp-import -n -p -f _site
